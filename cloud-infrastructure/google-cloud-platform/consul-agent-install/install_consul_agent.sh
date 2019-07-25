baa26d7a05099169544cc6d18d4896f327237c4b
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2019-07-25 11:17:51 +0100

    Improvements for v3 migration article

---
+++ cloud-infrastructure/google-cloud-platform/consul-agent-install/install_consul_agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+consul_version=1.5.2
+
+sudo apt install -y unzip
+
+
+# Install Consul
+wget -O /tmp/consul.zip "https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip"
+cd /tmp
+unzip -o ./consul.zip
+sudo mv -f ./consul /usr/local/bin/
+sudo mkdir -p /etc/consul.d
+sudo mkdir -p /etc/consul.d/data
+
+sudo tee /etc/systemd/system/consul.service > /dev/null <<"EOF"
+  [Unit]
+  Description = "Consul"
+
+  [Service]
+  KillSignal=INT
+  ExecStart=/usr/local/bin/consul agent -retry-join 'provider=k8s label_selector="app=consul,component=server" kubeconfig=/home/daniel/.kube/config' -data-dir=/etc/consul.d/data -config-dir=/etc/consul.d
+  Restart=always
+  Environment=GOOGLE_APPLICATION_CREDENTIALS=/home/daniel/.kube/creds.json
+EOF
+
+sudo systemctl enable consul.service
+sudo systemctl add-wants multi-user.target consul.service
+
+sudo systemctl daemon-reload
+sudo service consul restart
